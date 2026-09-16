b8816605344d61f1d322a8ad49bde998454fa5c2
jacob melendez <jacobmelendez.jm@gmail.com>
2026-09-16 19:47:45 +0200

    Finalize hero adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 export default function Page() {
   return (
     <>
-      <div className="flex h-[450px] w-screen items-center justify-center bg-[#f59e0b]">
-        <div className="h-[350px] w-[1200px] bg-[#e11d48]" />
+      <div className="flex h-screen w-screen items-center justify-center bg-[#f59e0b]">
+        <div className="flex h-full w-[1200px] items-center justify-center bg-[#e11d48]">
+          <p className="text-center text-base text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent free
+            libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+            imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+            Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
+            Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque
+            sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas
+            porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis
+            quis ligula lacinia aliquet. Mauris ipsum.
+          </p>
+        </div>
       </div>
       <section className="h-[450px] w-screen bg-[#16a34a]" />
       <section className="h-[450px] w-screen bg-[#c026d3]" />
